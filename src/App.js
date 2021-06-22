51137194ae32756297369f6eaa57a1821124f6b3
Ykub-Taras <ykub.taras@gmail.com>
2021-06-22 15:36:26 +0300

    HomeWork 01

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
+import Posts from "./components/posts/Posts";
 
 export default function App() {
     return (
         <div>
-
-
-
-
+            <Posts/>
         </div>
     );
 }
---
+++ src/components/posts/Posts.js
@@ -0,0 +1,27 @@
+import './Posts.css';
+import {useEffect, useState} from "react";
+
+export default function Posts() {
+    let [postsList, setPostsList] = useState([])
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(value => value.json())
+            .then(value => {
+                console.log(value);
+                setPostsList(value)
+            });
+    }, [])
+    return (
+        <div className={'postBlock'}>
+            {postsList.map(value =>
+                <div className={'block'}>
+                    <h3>User ID: {value.userId} | ID - {value.id}</h3>
+                    <h2>{value.title}</h2>
+                    <p>{value.body}</p>
+                    <hr/>
+                </div>)}
+        </div>
+    )
+
+
+}
